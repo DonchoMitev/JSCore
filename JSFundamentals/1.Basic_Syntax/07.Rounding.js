d7fdde1df9ded914e007aa3a76aafaaedb4de771
Doncho Mitev <doncho.mitev@mail.bg>
2019-01-14 17:29:10 +0200

    task rounding

---
+++ JSFundamentals/1.Basic_Syntax/07.Rounding.js
@@ -0,0 +1,11 @@
+function rounding(arr) {
+    let number = +arr[0];
+    let precision = +arr[1];
+    if (precision > 15) {
+        precision = 15;
+    }
+
+    console.log(+number.toFixed(precision));
+}
+
+rounding([10.5, 3]);
