2c3a9390544b9464356f7e8949b36a1c0d8d79e3
Doncho Mitev <doncho.mitev@mail.bg>
2019-01-14 15:52:44 +0200

    task distance time

---
+++ JSFundamentals/1.Basic_Syntax/02.RectangleArea.js
@@ -0,0 +1,9 @@
+function rectangle(a, b) {
+    let area = a * b;
+    let perimeter = 2*(a + b);
+
+    console.log(area.toFixed(2));
+    console.log(perimeter.toFixed(2));
+}
+
+rectangle(2.5, 3.14);
