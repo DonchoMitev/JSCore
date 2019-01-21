069b5f42b257b62e7d3c116528498f24c3aa1cc2
Doncho Mitev <doncho.mitev@mail.bg>
2019-01-21 18:26:08 +0200

    task - time walk

--- JSFundamentals/2.JS-Operators/08.TimeToWalk.js
+++ JSFundamentals/2.JS-Operators/08.TimeToWalk.js
@@ -3,8 +3,32 @@ function solve (numberSteps, footprintMeters, speedKmh) {
     let meters = numberSteps * footprintMeters;
     let speedMtrs = (speedKmh * 1000) / 3600;
 
-    let time = meters / speedMtrs;
+    let timeSeconds = Math.round(meters / speedMtrs);
 
-    let minute = time % 60;
-    let minute = 
+    let rest =Math.floor(meters / 500);
+
+    timeSeconds += rest * 60
+
+    let hour = 0;
+    let seconds = timeSeconds % 60;
+    let minutes = (timeSeconds - seconds) / 60;
+
+    printHour(hour, minutes, seconds);
+
+    function printHour(hour, minutes, seconds) {
+        if (hour < 10){
+            hour = "0" + hour;
+        } 
+        if (minutes < 10){
+            minutes = "0" + minutes;
+        } 
+        if (minutes < 10){
+            minutes = "0" + minutes;
+        }
+
+        console.log(`${hour}:${minutes}:${seconds}`);
+    }
+
 }
+
+solve(8000, 0.70, 5.5);
---
+++ JSFundamentals/2.JS-Operators/10.CaloriesObject.js
@@ -0,0 +1,19 @@
+function solve(arr) {
+
+    let obj = {};
+
+    for (let i = 0; i < arr.length; i+=2) {
+        let objKey = arr[i];
+        let objValue = +arr[i + 1];
+
+        obj[objKey] = objValue;
+    
+        
+    }
+
+    return obj;
+
+}
+
+
+console.log(solve(['Yoghurt', 48, 'Rise', 138, 'Apple', 52]));
