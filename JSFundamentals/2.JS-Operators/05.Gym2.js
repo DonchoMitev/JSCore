13eacb6294149d5b8cd57a7f57e3418a45b22bb1
Doncho Mitev <doncho.mitev@mail.bg>
2019-01-21 16:44:19 +0200

    task - greatest common div..

--- JSFundamentals/2.JS-Operators/05.Gym2.js
+++ JSFundamentals/2.JS-Operators/05.Gym2.js
@@ -3,32 +3,30 @@ function solve(day, service, time) {
     let price = 0;
 
     let daysOfWeek = function (day) {
-       return [ "Monday", 'Tuesday', "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].indexOf(day);
+        return ["Monday", 'Tuesday', "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].indexOf(day);
     };
 
     let serviceDayWeek = {
-        "Fitness" : 5,
-        "Sauna" : 4,
-        "Instructor" : 10
+        "Fitness": 5,
+        "Sauna": 4,
+        "Instructor": 10
     };
 
     let serviceWeekend = {
-        "Fitness" : 8,
-        "Sauna" : 8,
-        "Instructor" : 15
+        "Fitness": 8,
+        "Sauna": 8,
+        "Instructor": 15
     }
     if (daysOfWeek(day) <= 4) {
         if (time <= 15) {
             return serviceDayWeek[service];
-        }else{
+        } else {
             return serviceDayWeek[service] + 2.5;
         }
-        
-    }else{
+
+    } else {
         return serviceWeekend[service];
     }
-
-    
 
 }
 
---
+++ JSFundamentals/2.JS-Operators/06.GCD.js
@@ -0,0 +1,10 @@
+function gcd_rec(a, b) {
+    if (b) {
+        return gcd_rec(b, a % b);
+    } else {
+        return Math.abs(a);
+    }
+}
+
+
+console.log(gcd_rec(15, 5));
