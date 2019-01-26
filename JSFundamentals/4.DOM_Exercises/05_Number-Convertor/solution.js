4e32efe139d0eb18a8c382d60dbc97f606ce71f2
Doncho Mitev <doncho.mitev@mail.bg>
2019-01-26 15:06:19 +0200

    task - softuni quiz

--- JSFundamentals/4.DOM_Exercises/05_Number-Convertor/solution.js
+++ JSFundamentals/4.DOM_Exercises/05_Number-Convertor/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-//TODO
+    
+    let number = document.getElementById('input').value;
+
+    let menuOptions = document.getElementById('selectMenuTo');
+   
+
+    let optionOne = document.createElement('option');
+    let optionTwo = document.createElement('option');
+    
+    menuOptions.appendChild(optionOne).textContent = "Binary";
+    menuOptions.appendChild(optionTwo).textContent = "Hexadeicmala";
+
+    let options = document.getElementsByTagName('option')[2];
+    console.log(options);
+
+
+    if (options.value = "selected") {
+        console.log("hexa");
+    }
+        
+    // }else if (options[3].selected) {
+    //     console.log("binary");
+    // }
+   
+    
+
 }
--- JSFundamentals/4.DOM_Exercises/06_SoftUni-Quizz/solution.js
+++ JSFundamentals/4.DOM_Exercises/06_SoftUni-Quizz/solution.js
@@ -1,3 +1,59 @@
 function solve() {
-	//TODO
+	let buttons = Array.from(document.getElementsByTagName('button'));
+	let sections = Array.from(document.getElementsByTagName('section'));
+
+	buttons[0].addEventListener('click', firstEvent);
+	buttons[1].addEventListener('click', secondEvent);
+	buttons[2].addEventListener('click', resultEvent);
+
+	let firstAnsewr = document.getElementsByName('softUniYear');
+	let secondAnsewr = document.getElementsByName('popularName');
+	let thirdAnsewr = document.getElementsByName('softUniFounder');
+	let rightAnswer = 0;
+
+	sections[0].style.display = 'block';
+
+	function firstEvent() {
+
+		for	(let btn of firstAnsewr) {
+			if (btn.checked) {
+				if (btn.value == "2013") {
+					rightAnswer++;
+				}
+				
+				sections[1].style.display = 'block';
+				buttons[0].removeEventListener();
+			}
+		}  
+	}
+
+	function secondEvent () {
+		for (let btn2 of secondAnsewr) {
+			if (btn2.checked) {
+				if(btn2.value == "Pesho") {
+					rightAnswer++;
+				}
+				
+				sections[2].style.display = 'block';
+				buttons[1].removeEventListener();
+			}
+		}
+	}
+
+	function resultEvent () {
+		for (let btn of thirdAnsewr) {
+			if (btn.checked) {
+				if(btn.value == "Nakov"){
+					rightAnswer++;
+				}
+				
+				let result = document.getElementById('result');
+
+				result.textContent = rightAnswer === 3 ? "You are recognized as top SoftUni fan!" : `You have ${+rightAnswer} right answers`
+				buttons[2].removeEventListener();
+			}
+
+		}
+	}
+
 }
