3a6cf23192c5a4bcc6493c5a7b08abaf2cbe2d6e
Doncho Mitev <doncho.mitev@mail.bg>
2019-01-27 11:54:55 +0200

    task - toto numbers

--- JSFundamentals/4.DOM_Exercises/08_Toto-Numbers/solution.js
+++ JSFundamentals/4.DOM_Exercises/08_Toto-Numbers/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-	//TODO
+	let numbers = document.querySelector('input[type="text"]');
+	let button = document.querySelector('button');
+
+	button.addEventListener('click', event);
+
+	function event() {
+		let allNmbers = numbers.value.split(' ').map((e) => {return +e});
+		let fillterResult = allNmbers.filter((e) => {return e <= 49 && e > 0});
+
+		if (allNmbers.length == 6 && fillterResult.length == 6) {
+			let parent = document.getElementById('allNumbers');
+			for (let i = 1; i <= 49; i++) {
+				let div = document.createElement('div');
+				div.textContent = `${i}`;
+				div.setAttribute('class', 'numbers');
+		
+				if (allNmbers.includes(i)) {
+					div.style.background = 'orange'
+				}
+				parent.appendChild(div);
+				
+			}
+			button.disabled = true;
+			document.getElementsByTagName('input')[0].setAttribute('disabled', 'true');
+		}
+		
+	}
 }
