32668b78b04d9dc7c776a9fbe3a29130e2dbae14
Doncho Mitev <doncho.mitev@mail.bg>
2019-01-27 18:17:33 +0200

    task - cooking number

--- JSFundamentals/6.FunctionAndLogicFlow_Exercises/04_Cooking-Numbers/solution.js
+++ JSFundamentals/6.FunctionAndLogicFlow_Exercises/04_Cooking-Numbers/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-    //TODO...
+    
+    let buttons = Array.from(document.querySelectorAll('#exercise #operations button')).forEach((btn) => {
+        btn.addEventListener('click', event);
+    });
+
+    function event (ev) {
+        let btn = ev.target;
+        let number = document.querySelector('#exercise input');
+        if (number == '') {
+            number = 0;
+        }
+        let output = document.querySelector('#exercise p');
+
+        if (btn.textContent == "Chop") {
+            output.textContent = +number.value / 2;
+        }else if (btn.textContent == "Dice") {
+            output.textContent = Math.sqrt(+number.value);
+        }else if (btn.textContent == "Spice") {
+            output.textContent = +number.value + 1;    
+        }else if (btn.textContent == "Bake") {
+            output.textContent = +number.value * 3;
+        }else if (btn.textContent == "Fillet") {
+            output.textContent = +number.value * 0.80;
+        }
+
+        number.value = +output.textContent;
+    }
 }
