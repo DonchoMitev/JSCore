16fd0cd76279ec6c3c84d762003b116c1c61cf51
Doncho Mitev <doncho.mitev@mail.bg>
2019-01-27 16:28:31 +0200

    task - check number is valid ot not

--- JSFundamentals/6.FunctionAndLogicFlow_Exercises/02_Simple-Number-Validator/solution.js
+++ JSFundamentals/6.FunctionAndLogicFlow_Exercises/02_Simple-Number-Validator/solution.js
@@ -1,3 +1,33 @@
 function validate() {
-    // TODO:
+
+    let arr = [2, 4, 8, 5, 10, 9, 7, 3, 6];
+    let sum = 0;
+    
+    let btn = document.querySelector('#exercise > fieldset > div > button').addEventListener('click', checkNumber);
+
+    function checkNumber(){
+        let number = document.querySelector('#exercise > fieldset > div > input').value;
+
+        let lastDigit = number[number.length - 1];
+
+        for (let i = 0; i < 9; i++) {
+            let temp1 = number[i];
+            let temp2 = arr[i];
+
+            sum += temp1 * temp2;
+        }
+
+        let reminder = sum % 11;
+
+        if(reminder === 10) {
+            reminder = 0;
+        }
+
+        if (+lastDigit === reminder) {
+            document.querySelector('#response').textContent = 'This number is Valid!';
+        }else {
+            document.querySelector('#response').textContent = 'This number is NOT Valid!';
+        }
+    }
+
 }
