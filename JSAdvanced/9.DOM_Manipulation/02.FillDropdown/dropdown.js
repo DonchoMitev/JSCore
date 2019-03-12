a48cb7eb09a3f35e0189661a7a0160699edf696d
Doncho Mitev <doncho.mitev@mail.bg>
2019-03-12 15:04:56 +0200

    task - dropdown menu

---
+++ JSAdvanced/9.DOM_Manipulation/02.FillDropdown/dropdown.js
@@ -0,0 +1,20 @@
+function addItem() {
+    let text = $('#newItemText').val();
+    let value = $('#newItemValue').val();
+    let menu = $('#menu');
+
+    let optioneElement = $('<option>');
+    optioneElement.val(value);
+    optioneElement.text(text);
+
+    menu.append(optioneElement);
+    reset();
+
+    function reset() {
+        $('#newItemText').val('');
+        $('#newItemValue').val('');
+    }
+
+
+    console.log(text, value);
+}
